bbb5698598785a51e4a924c6d260d083df9ac71f
Ram-Goutham <brgowtham97@gmail.com>
2025-03-04 00:08:25 +0530

    0007

--- todolist.js
+++ todolist.js
@@ -2,9 +2,9 @@ let todoInput = document.getElementsByClassName("todoInput");
 let todoOutput = document.getElementsByClassName("todoOutput");
 let newTask = document.getElementById("newTask");
 
-function createLi(todoInput){
+function createLi(input){
     let li = document.createElement("li");
-    li.innerHTML = todoInput;
+    li.innerHTML = input;
 }
 
 function buttonInput(){
