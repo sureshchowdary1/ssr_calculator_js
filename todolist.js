5857bf4fe50342830da77388a805726e73a6576f
Ram-Goutham <brgowtham97@gmail.com>
2025-03-03 23:47:50 +0530

    2347

--- todolist.js
+++ todolist.js
@@ -1,5 +1,5 @@
 let todoInput = document.getElementsByClassName("todoInput");
 let todoOutput = document.getElementsByClassName("todoOutput");
 function buttonInput(){
-    todoOutput.value = todoOutput;
+    todoOutput.innerHTML = todoInput.value;
 }
